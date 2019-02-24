35cc5d9b843f94a71bc8e1a1b892e02ed985f0d1
blurHY <blurhy@outlook.com>
2019-02-24 12:40:21 +0800

    Basic features

---
+++ index.js
@@ -0,0 +1,35 @@
+const del = require('del');
+const ncp = require('ncp').ncp;
+const path = require("path")
+
+hexo.extend.deployer.register('zeronet', function (args, callback) {
+    if (!args.siteAddr || !args.zeronetDir) {
+        console.log(`
+        Config it in root _config.yml
+        Example:
+
+        deploy:
+            type: zeronet
+            siteAddr: 12q9YNb5oJ331nTjUxWefV9W2EXpMu8RWi
+            zeronetDir: D:/ZeroNet/
+        `)
+        callback()
+    }
+    else {
+        const sitedir = path.join(args.zeronetDir, "data", args.siteAddr)
+        del.sync([path.join(sitedir, "**"),
+        `!${path.join(sitedir, "content.json")}`,
+        `!${path.join(sitedir, "dbschema.json")}`,
+        `!${path.join(sitedir, "data", "**")}`,
+        `!${path.join(sitedir, "data")}`,
+        `!${path.join(sitedir, "data-default")}`,
+        `!${path.join(sitedir, "data-default", "**")}`
+        ], { force: true, dryRun: args.dryRun })
+        ncp(hexo.public_dir, sitedir, err => {
+            if (err)
+                console.log(err)
+            callback()
+        })
+        // TODO: Auto publish site
+    }
+})
